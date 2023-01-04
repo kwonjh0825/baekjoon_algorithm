b4ef0bdb4a83c2e72de0eb3b1e796b2dfd4ccee8
Junhyuk Kwon <63788871+kwonjh0825@users.noreply.github.com>
2023-01-04 18:11:53 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 52 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -0,0 +1,13 @@
+n, m = map(int, input().split())
+arr1, arr2 = [], []
+for i in range(n):
+    arr1.append(list(map(int, input().split())))
+
+for i in range(n):
+    arr2.append(list(map(int, input().split())))
+
+for i in range(n):
+    for j in range(m):
+        arr1[i][j] += arr2[i][j]
+        print(arr1[i][j], end=' ')
+    print()
