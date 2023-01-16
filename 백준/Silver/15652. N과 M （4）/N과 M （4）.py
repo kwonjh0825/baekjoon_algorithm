f65ed238d450c1a90946d15c59ab1d4f7a1a4d8b
Junhyuk Kwon <63788871+kwonjh0825@users.noreply.github.com>
2023-01-16 16:25:09 +0900

    [Silver III] Title: N과 M (4), Time: 48 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,13 @@
+n, m = map(int, input().split())
+s = []
+
+def dfs(t):
+    if len(s) == m:
+        print(' '.join(map(str, s)))
+        return
+
+    for i in range(t, n+1):
+        s.append(i)
+        dfs(i)
+        s.pop()
+dfs(1)
