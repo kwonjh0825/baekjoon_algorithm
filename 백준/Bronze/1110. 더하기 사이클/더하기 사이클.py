a9e41d4ba1e1225ccd553f50742f252a79d57bd2
Junhyuk Kwon <63788871+kwonjh0825@users.noreply.github.com>
2023-01-17 14:08:34 +0900

    [Bronze I] Title: 더하기 사이클, Time: 44 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
@@ -1,17 +1,9 @@
-n = int(input())
-if n < 10:
-    list_n = [0, n]
-else:
-    list_n = [int (a) for a in str(n)]
-origin_n = list_n
-t = cycle = int()
-while(1):
-    t = [int(b) for b in str(sum(list_n))]
-    if sum(list_n) < 10:
-        list_n = [list_n[1], t[0]]
-    else:
-        list_n = [list_n[1], t[1]]
-    cycle += 1
-    if list_n == origin_n:
-        break
-print(cycle)
+N = input()
+if len(N) < 2:
+    N = '0' + N
+new_num = N[-1] + str(sum(list(map(int, N))))[-1]
+cnt = 1
+while new_num != N:
+    new_num = new_num[-1] + str(sum(list(map(int, new_num))))[-1]
+    cnt += 1
+print(cnt)
