9914c6912b31cabbdbcdf5890c257de78c59035b
Junhyuk Kwon <63788871+kwonjh0825@users.noreply.github.com>
2023-01-17 13:36:51 +0900

    [Bronze III] Title: 최댓값, Time: 32 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.py
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -1,9 +1,8 @@
-a = [int(input()) for _ in range(9)]
-print(max(a))
-ind = 1
-
-for j in range(9):
-    if max(a) == a[j]:
-        ind += j
-        break
-print(ind)
+li = [int(input()) for _ in range(9)]
+max = 0
+idx = 0
+for i, l in enumerate(li):
+    if max < l:
+        max = l
+        idx = i + 1
+print(max, idx, sep='\n')
