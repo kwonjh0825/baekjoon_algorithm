753776e3b6b360c1a5c69724862fb3cbdf4610b3
Junhyuk Kwon <63788871+kwonjh0825@users.noreply.github.com>
2023-01-18 14:00:41 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 128 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
@@ -1,6 +1,8 @@
-T = int(input())
+import sys
+
+T = int(sys.stdin.readline())
 for _ in range(T):
-    li = list(input().split())
+    li = list(sys.stdin.readline().split())
     for l in li:
         print(l[::-1], end=' ')
     print()
