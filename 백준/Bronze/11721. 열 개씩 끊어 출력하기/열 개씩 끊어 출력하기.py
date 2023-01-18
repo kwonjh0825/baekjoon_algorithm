0d88bc4b74b52ea369c62ab7773c76a70c2fcef6
Junhyuk Kwon <63788871+kwonjh0825@users.noreply.github.com>
2023-01-18 14:10:39 +0900

    [Bronze III] Title: 열 개씩 끊어 출력하기, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/11721. 열 개씩 끊어 출력하기/열 개씩 끊어 출력하기.py
+++ 백준/Bronze/11721. 열 개씩 끊어 출력하기/열 개씩 끊어 출력하기.py
@@ -1,7 +1,3 @@
 string = input()
-for i in range(0, 100, 10):
-    s = string[i:i+10]
-    if s:
-        print(s)
-    else:
-        break
+for i in range(0, len(string), 10):
+    print(string[i:i+10])
