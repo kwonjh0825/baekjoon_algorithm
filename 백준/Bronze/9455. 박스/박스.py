c8fd8e1c57a7e529d50b523702f6ac86d174367a
Junhyuk Kwon <63788871+kwonjh0825@users.noreply.github.com>
2023-01-31 14:43:47 +0900

    [Bronze I] Title: 박스, Time: 336 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/9455. 박스/박스.py
@@ -0,0 +1,18 @@
+T = int(input())
+
+for _ in range(T):
+    box = []
+    ans = 0
+    m, n = map(int, input().split())
+    for _ in range(m):
+        box.append(list(map(int, input().split())))
+    
+    for i in range(n):
+        cnt = 0
+        for j in range(m-1, -1, -1):
+            if box[j][i]:
+                ans += cnt
+            else:
+                cnt += 1
+    
+    print(ans)
