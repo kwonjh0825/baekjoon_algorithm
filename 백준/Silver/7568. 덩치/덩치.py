e5aadd9a953251886583b3db953b96c69578fffb
Junhyuk Kwon <63788871+kwonjh0825@users.noreply.github.com>
2023-02-07 13:54:36 +0900

    [Silver V] Title: 덩치, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.py
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -4,12 +4,10 @@ cnt_list = []
 for i in range(n):
     li.append(list(map(int, input().split())))
 for i in li:
-    cnt = 0
+    cnt = 1
     for j in li:
         if i[0] < j[0] and i[1] < j[1]:
             cnt += 1
-    cnt += 1
     cnt_list.append(cnt)
 
-for i in cnt_list:
-    print(i, end=' ')
+print(*cnt_list)
